14ce16c0c529485099ffb16a6f309732230802d1
Macky Dieng <mdieng@scalair.fr>
2018-02-01 13:46:14 +0100

    Performing refactoring in the code

--- js/functions.js
+++ js/functions.js
@@ -10,88 +10,87 @@ function activePlayer() {
 *  @cls : classe recherchée parmi les clesses de l'élément
 ****/
 function hasClass(element, cls) {
-    return (element.className.split(" ")).indexOf(cls) > -1;
+  return (element.className.split(" ")).indexOf(cls) > -1;
 }
 /**
  * Renvoie les éléments suivants d'un élément donné
  * @param elmt : l'élément auquel on cherche les éléments suivants
- **/   
+ **/
 function getAllNextElmts(elmt) {
-    var next  = elmt.nextElementSibling;
-    //var next = elmt.nextElementSibling;
-    var elmtContainer = [];
-    if (next !== undefined) {
-        while (next && next.nodeType === 1) {
-            elmtContainer.push(next);
-            next = next.nextElementSibling;
-        }
-        return elmtContainer;
-    } else {
-        console.log("Impossible to trace road");
+  var next  = elmt.nextElementSibling;
+  //var next = elmt.nextElementSibling;
+  var elmtContainer = [];
+  if (next !== undefined) {
+    while (next && next.nodeType === 1) {
+      elmtContainer.push(next);
+      next = next.nextElementSibling;
     }
+    return elmtContainer;
+  } else {
+    console.log("Impossible to trace road");
+  }
 }
 //Renvoie les éléments précédents d'un élément donné
 function getAllPrevElmts(elmt) {
-    var prev = elmt.previousElementSibling;
-    var elmtContainer = [];
-    if (prev !== undefined) {
-        while (prev && prev.nodeType === 1) {
-            elmtContainer.push(prev);
-            prev = prev.previousElementSibling;
-        }
-        return elmtContainer;
-    } else {
-        console.log("Impossible to trace road");
+  var prev = elmt.previousElementSibling;
+  var elmtContainer = [];
+  if (prev !== undefined) {
+    while (prev && prev.nodeType === 1) {
+      elmtContainer.push(prev);
+      prev = prev.previousElementSibling;
     }
+    return elmtContainer;
+  } else {
+    console.log("Impossible to trace road");
+  }
 }
 //Renvoie les élements suivants du parent d'un élément donné
 function getAllParentNextElmts(elmt) {
-    var parentNext = elmt.parentElement.nextElementSibling;
-    var elmtContainer = [];
-    if (parentNext !== undefined) {
-        while (parentNext && parentNext.nodeType === 1) {
-            elmtContainer.push(parentNext);
-            parentNext = parentNext.nextElementSibling;
-        }
-        return elmtContainer;
-    } else {
-        console.log("Impossible to trace road");
+  var parentNext = elmt.parentElement.nextElementSibling;
+  var elmtContainer = [];
+  if (parentNext !== undefined) {
+    while (parentNext && parentNext.nodeType === 1) {
+      elmtContainer.push(parentNext);
+      parentNext = parentNext.nextElementSibling;
     }
+    return elmtContainer;
+  } else {
+    console.log("Impossible to trace road");
+  }
 }
 //Renvoie les élements précédents du parent d'un élément donné
 function getAllParentPrevElmts(elmt) {
-    var parentPrev = elmt.parentElement.previousElementSibling;
-    var elmtContainer = [];
-    if (parentPrev !== undefined) {
-        while (parentPrev && parentPrev.nodeType === 1) {
-            elmtContainer.push(parentPrev);
-            parentPrev = parentPrev.previousElementSibling;
-        }
-        return elmtContainer;
-    } else {
-        console.log("Impossible to trace road");
+  var parentPrev = elmt.parentElement.previousElementSibling;
+  var elmtContainer = [];
+  if (parentPrev !== undefined) {
+    while (parentPrev && parentPrev.nodeType === 1) {
+      elmtContainer.push(parentPrev);
+      parentPrev = parentPrev.previousElementSibling;
     }
+    return elmtContainer;
+  } else {
+    console.log("Impossible to trace road");
+  }
 }
 //Permet d'afficher les messages d'activités en temps réel dans la console du jeu
 function display (message) {
-	
 	document.getElementById("activity-area-content").textContent = message;
 }
 //Permet de réinitialiser les routes
 function resetRoadCells (weapons) {
-    for (var i = 0; i < 10; i++) {
-        for (var j = 0; j < 10; j++) {
-            var elmt = document.getElementById("X"+i+"Y"+j);
-            if (elmt.getAttribute("class") == "box road") {
-                elmt.setAttribute("class", "box");
-                elmt.style.background = "url('img/sol.png') no-repeat scroll 0 0 / 100% auto rgba(0, 0, 0, 0)";
-            }
-            for (k in weapons) {
-                if (elmt.getAttribute("class") == "box weapon" + " " + weapons[k].getName() + " " + "road") {
-                    elmt.setAttribute("class", "box weapon"+ " "+ weapons[k].getName());
-                    elmt.style.background = "url('img/"+ weapons[k].getVisuel() +"') no-repeat scroll 0 0 / 100% auto rgba(0, 0, 0, 0)";
-                }
-            }
-        };
+  for (var i = 0; i < 10; i++) {
+    for (var j = 0; j < 10; j++) {
+      var elmt = document.getElementById("X"+i+"Y"+j);
+      if (elmt.getAttribute("class") == "box road") {
+        elmt.setAttribute("class", "box");
+        elmt.style.background = "url('img/sol.png') no-repeat scroll 0 0 / 100% auto rgba(0, 0, 0, 0)";
+      }
+      for (k in weapons) {
+        if (elmt.getAttribute("class") == "box weapon" + " " + weapons[k].getName() + " " + "road") {
+          elmt.setAttribute("class", "box weapon"+ " "+ weapons[k].getName());
+          elmt.style.background = "url('img/"+ weapons[k].getVisuel() +"') no-repeat scroll 0 0 / 100% auto rgba(0, 0, 0, 0)";
+        }
+      }
     };
+  };
 }
--- js/interface.js
+++ js/interface.js
@@ -1,5 +1,5 @@
 function Interface () {
-	
+
 	//Créations des armes et joueurs
 	var bombe = new Weapon('bombe', 20, 'bombe.jpg','bombe300.png');
 	var missile = new Weapon('missile', 25, 'missile.jpg','missile300.png');
@@ -12,25 +12,21 @@ function Interface () {
 
 	var weaponsArray = [bombe, missile, carapaceBleu, klaxon, carapaceVerte];
 	var playersArray = [mario,bowser];
-	
+
 	var roadImage = "url('img/1.png') no-repeat scroll 0 0 / 100% auto rgba(0, 0, 0, 0)";
 
-	
-
-	/** 
+	var self = this;
+	/**
 	* Permet de générer des coordonnées aléatoires X,Y du tableau de jeu
 	* @param maxNumber : Nombre d'obstavles à placer sur le jeu
 	**/
-	this.lunchGame = function () {
-
+	this.launchGame = function () {
 		this.putWeapons();
-		this.putPlayers();
+		this.putPlayersOnStage();
 		//Activation du joueur courant
 		this.activeAnPlayer();
+		//traçage des routes autour du joueur
 		this.traceMoveDirection();
-
-
-		//this.getGameInstance();
 	}
 	this.getWeapons = function () {
 		return weaponsArray;
@@ -38,26 +34,20 @@ function Interface () {
 	this.getPlayers = function () {
 		return playersArray;
 	}
-	/*this.getGameInstance = function() {
-		var game = new Game();
-		game.move(playersArray,weaponsArray,this);
-	}*/
 	this.getRandomId = function (maxNumber) {
-	    var posX = Math.floor(Math.random() * maxNumber);
-	    var posY = Math.floor(Math.random() * maxNumber);
-	    var id = "X"+posX+"Y"+posY;
-	    var currentElement = document.getElementById(id);
+    var posX = Math.floor(Math.random() * maxNumber);
+    var posY = Math.floor(Math.random() * maxNumber);
+    var id = "X"+posX+"Y"+posY;
+    var currentElement = document.getElementById(id);
 		var classs = currentElement.className.split(" ");
-	    return {Id : id, classs : classs, elmt : currentElement };
+    return {Id : id, classs : classs, elmt : currentElement };
 	}
 	/****
 	*	Permet de placer des éléments sur le plateau de jeu (armes ou joueurs)
 	***/
 	this.put = function(objArray, objIndexArray, nbIteration, objType) {
-
 		var idArray = new Array();
 		var i = 0;
-
 		while (i < nbIteration) {
 			var obj = this.getRandomId(10);
 			var objId = Math.floor(Math.random() * nbIteration);
@@ -73,31 +63,30 @@ function Interface () {
 						obj.elmt.className = obj.elmt.className + " " + "player" + " " + objArray[objId].getFirstName();
 					} else { //Si l'emplacement est celui d'une arme, alors on regenère un nouveau emplacement pour le joueur
 						while (!isNotExistElement) {
-			                obj = this.getRandomId(10)
-			                var objId = Math.floor(Math.random() * nbIteration);
-			                //L'on vérifie que l'id de de l'emplacement n'a pas encore été généré et que sa classe n'est pas "wrongCell"
-			                //et que elle n'est "weapon" n'ont plus, et que son index n'a pas été déjà généré.
-			                if (idArray.indexOf(obj.Id) === -1 && obj.classs.indexOf("wrongCell") === -1 && 
-			                	obj.classs.indexOf("weapon") === -1 && objIndexArray.indexOf(objId) === -1) {
-
-			                    isNotExistElement = true;
-			                }
-			            }
+	            obj = this.getRandomId(10)
+	            var objId = Math.floor(Math.random() * nbIteration);
+	            //L'on vérifie que l'id de de l'emplacement n'a pas encore été généré et que sa classe n'est pas "wrongCell"
+	            //et que elle n'est "weapon" n'ont plus, et que son index n'a pas été déjà généré.
+	            if (idArray.indexOf(obj.Id) === -1 && obj.classs.indexOf("wrongCell") === -1 &&
+	            	obj.classs.indexOf("weapon") === -1 && objIndexArray.indexOf(objId) === -1) {
+                isNotExistElement = true;
+	            }
+	          }
 						obj.elmt.style.background = "url('img/" + objArray[objId].getProfil()[1] + "')" + " " + "no-repeat scroll 0 0 / 100% auto rgba(0, 0, 0, 0)";
 						obj.elmt.className = obj.elmt.className + " " + "player" + " " + objArray[objId].getFirstName();
 					}
 				}
 			} else { //Ici l'emplacement généré aléatoirement existe déjà, donc on regénère un autre
-				var isNotExistElement = false;
+					var isNotExistElement = false;
 			    //on itère jusqu'à ce que l'on génère un élément qui n'a pas été encore généré.
-	            while (!isNotExistElement) {
-	                obj = this.getRandomId(10)
-	                var objId = Math.floor(Math.random() * nbIteration);
-	                if (idArray.indexOf(obj.Id) === -1 && obj.classs.indexOf("wrongCell") === -1 && objIndexArray.indexOf(objId) === -1) {
-	                    isNotExistElement = true;
-	                }
-	            }
-	            if (objType==="weapon") {
+          while (!isNotExistElement) {
+            obj = this.getRandomId(10)
+            var objId = Math.floor(Math.random() * nbIteration);
+            if (idArray.indexOf(obj.Id) === -1 && obj.classs.indexOf("wrongCell") === -1 && objIndexArray.indexOf(objId) === -1) {
+              isNotExistElement = true;
+            }
+          }
+          if (objType==="weapon") {
 					obj.elmt.style.background = "url('img/" + objArray[objId].getVisuel() + "')" + " " + "no-repeat scroll 0 0 / 100% auto rgba(0, 0, 0, 0)";
 					obj.elmt.className = obj.elmt.className + " " + "weapon" + " " + objArray[objId].getName();
 				} else {
@@ -129,39 +118,35 @@ function Interface () {
 		this.put(newWeaponsArray, weaponsIndexArray, newWeaponsArray.length, "weapon");
 	}
 	/**
-	* Permet de placer les joueurs sur le plateau
-	**/
-	this.putPlayers = function () {
-
-		var playersIndexArray = new Array();
-
-		//placement des informations par défaut du joueur 1 de façon dynamique
-		document.getElementById("player1-profil-img").src = 'img/' + mario.getProfil()[0];
-		document.getElementById("player1-name").textContent = mario.getFirstName();
-		document.getElementById("player1-life-point").textContent = mario.getLifePoint();
-		//Information sur l'arme du joueur 1
-		document.getElementById("player1-weapon-visuel").src = 'img/' + carapaceVerte.getVisuel();
-		document.getElementById("player1-weapon-name").textContent = carapaceVerte.getName();
-		document.getElementById("player1-degat").textContent = carapaceVerte.getDamage();
-
-		//placement des informations par défaut du joueur 2 de façon dynamique
-		document.getElementById("player2-profil-img").src = 'img/' + bowser.getProfil()[0];
-		document.getElementById("player2-name").textContent = bowser.getFirstName();
-		document.getElementById("player2-life-point").textContent = bowser.getLifePoint();
-
-		//Information sur l'arme du joueur 2
-		document.getElementById("player2-weapon-visuel").src = 'img/' + carapaceVerte.getVisuel();
-		document.getElementById("player2-weapon-name").textContent = carapaceVerte.getName();
-		document.getElementById("player2-degat").textContent = carapaceVerte.getDamage();
-
-		this.put(playersArray, playersIndexArray, playersArray.length, "player");		
+	 * [Bind players values to html elements]
+	 */
+	this.assignPlayerInfos = function () {
+		for (var i = 0; i < playersArray.length; i++) {
+			var player = playersArray[i];
+			var index = i + 1; /*because we dos'nt have 0 as html elmt id index*/
+			/*Informations about the player*/
+			document.getElementById("player"+ index + "-profil-img").src = 'img/' + player.getProfil()[0];
+			document.getElementById("player"+ index + "-name").textContent = player.getFirstName();
+			document.getElementById("player"+ index + "-life-point").textContent = player.getLifePoint();
+			/*Informations about player weapon*/
+			document.getElementById("player"+ index + "-weapon-visuel").src = 'img/' + player.weapon.getVisuel();
+			document.getElementById("player"+ index + "-weapon-name").textContent = player.weapon.getName();
+			document.getElementById("player"+ index + "-degat").textContent = player.weapon.getDamage();
+		}
 	}
-	/***
-	* Permet de tracer la direction de déplacement des joueurs (les routes)
-	**/
+	/**
+	 * [Put all players on the stage]
+	 */
+	this.putPlayersOnStage = function () {
+		var playersIndexArray = new Array();
+		self.assignPlayerInfos();
+		this.put(playersArray, playersIndexArray, playersArray.length, "player");
+	}
+	/**
+	 * [Allows to trace players move direction]
+	 * @return {[type]} [description]
+	 */
 	this.traceMoveDirection = function () {
-		
-		
 		//Récupération de la position du  joueur courant
 		var activePlayer = document.getElementsByClassName("active")[0];
 		var tabNextElmts = getAllNextElmts(activePlayer);
@@ -207,140 +192,70 @@ function Interface () {
 			}
 		}
 	}
+	this.traceRoadByIndex = function (tabElmts, index) {
+		if (hasClass(tabElmts[index],"weapon")) {
+			for (i in weaponsArray) {
+				if (hasClass(tabElmts[index], weaponsArray[i].getName())) {
+					tabElmts[index].style.background = "url('img/" + weaponsArray[i].getRoadImage() + "')"  + " " +"no-repeat scroll 0 0 / 100% auto rgba(0, 0, 0, 0)";
+					tabElmts[index].className = tabElmts[index].className + " " + "road";
+				}
+			}
+		} else {
+			tabElmts[index].style.background = roadImage;
+			tabElmts[index].className = tabElmts[index].className + " " + "road";
+		}
+	}
 	/**
 	* Permet de tracer les routes à droite et à gauche du joueur
 	* tabElmts : tableau des éléments (précédents ou suivants)
 	****/
-
 	this.traceLeftRightRoad = function (tabElmts) {
-
 		if (tabElmts.length === 1) {
 			if (!(hasClass(tabElmts[0],"wrongCell")) && !(hasClass(tabElmts[0],"player"))) {
-				if (hasClass(tabElmts[0],"weapon")) {
-					for (i in weaponsArray) {
-						if (hasClass(tabElmts[0], weaponsArray[i].getName())) {
-							tabElmts[0].style.background = "url('img/" + weaponsArray[i].getRoadImage() + "')"  + " " +"no-repeat scroll 0 0 / 100% auto rgba(0, 0, 0, 0)";
-							tabElmts[0].className = tabElmts[0].className + " " + "road";
-						}
-					}
-				} else {
-					tabElmts[0].style.background = roadImage;
-					tabElmts[0].className = tabElmts[0].className + " " + "road";
-				}
-			} else {
-				return;
-			}
+				self.traceRoadByIndex(tabElmts, 0);
+			} else { return; }
 		} else if (tabElmts.length === 2 || tabElmts.length === 3) {
 			for (var i = 0; i < tabElmts.length; i++) {
 				if (!(hasClass(tabElmts[i],"wrongCell")) && !(hasClass(tabElmts[i],"player"))) {
-					if (hasClass(tabElmts[i],"weapon")) {
-						for (j in weaponsArray) {
-							if (hasClass(tabElmts[i],weaponsArray[j].getName())) {
-								tabElmts[i].style.background = "url('img/" + weaponsArray[j].getRoadImage() + "')" + " " +"no-repeat scroll 0 0 / 100% auto rgba(0, 0, 0, 0)";
-								tabElmts[i].className = tabElmts[i].className + " " + "road";
-							}
-						}
-					} else {
-						tabElmts[i].style.background = roadImage;
-						tabElmts[i].className = tabElmts[i].className + " " + "road";
-					}
-				} else {
-					return;	
-				}
+					self.traceRoadByIndex(tabElmts, i);
+				} else { return; }
 			}
 		} else if (tabElmts.length > 3 ) {
 			for (var i = 0; i < 3; i++) {
 				if (!(hasClass(tabElmts[i],"wrongCell")) && !(hasClass(tabElmts[i],"player"))) {
-					if (hasClass(tabElmts[i],"weapon")) {
-						for (j in weaponsArray) {
-							if (hasClass(tabElmts[i],weaponsArray[j].getName())) {
-								tabElmts[i].style.background = "url('img/" + weaponsArray[j].getRoadImage() + "')" + " " +"no-repeat scroll 0 0 / 100% auto rgba(0, 0, 0, 0)";
-								tabElmts[i].className = tabElmts[i].className + " " + "road";
-							}
-						}
-					} else {
-						tabElmts[i].style.background = roadImage;
-						tabElmts[i].className = tabElmts[i].className + " " + "road";
-					}
-				} else {
-					return;
-				}
+					self.traceRoadByIndex(tabElmts, i);
+				} else { return; }
 			}
-		} else {
-			console.log("No road is possible to trace");
-		}
+		} else { console.log("No road is possible to trace"); }
 	}
 	this.traceTopBottomRoad = function(tabElmts,activeElementIndex) {
+		perform2Loop = function (length) {
+			for (let i = 0; i < length; i++) {
+				let childrens = tabElmts[i].children;
+				for (let j = 0; j < childrens.length; j++) {
+					//Vérication de l'index de l'élément actif par rapport aux différents éléments
+					if (j === activeElementIndex) {
+						if (!(hasClass(childrens[j],"wrongCell")) && !(hasClass(childrens[j],"player"))) {
+							self.traceRoadByIndex(childrens, j);
+						} else { return; }
+					}
+				}
+			}
+		}
 		if (tabElmts.length === 1) {
 			var childrens = tabElmts[0].children;
 			for (var i = 0; i < childrens.length; i++) {
 				//Vérication de l'index de l'élément actif par rapport aux différents éléments
 				if (i === activeElementIndex) {
 					if (!(hasClass(childrens[i],"wrongCell")) && !(hasClass(childrens[i],"player"))) {
-						if (hasClass(childrens[i],"weapon")) {
-							for (j in weaponsArray) {
-								if (hasClass(childrens[i], weaponsArray[j].getName())) {
-									childrens[i].style.background = "url('img/" + weaponsArray[j].getRoadImage() + "')"  + " " +"no-repeat scroll 0 0 / 100% auto rgba(0, 0, 0, 0)";
-									childrens[i].className = childrens[i].className + " " + "road";
-								}
-							}
-						} else {
-							childrens[i].style.background = roadImage;
-							childrens[i].className = childrens[i].className + " " + "road";
-						}
-					} else {
-						return;
-					}
+						self.traceRoadByIndex(childrens, i);
+					} else { return; }
 				}
 			}
 		} else if (tabElmts.length === 2 || tabElmts.length === 3) {
-			for (var i = 0; i < tabElmts.length; i++) {
-				var childrens = tabElmts[i].children;
-				for (var j = 0; j < childrens.length; j++) {
-					//Vérication de l'index de l'élément actif par rapport aux différents éléments
-					if (j === activeElementIndex) {
-						if (!(hasClass(childrens[j],"wrongCell")) && !(hasClass(childrens[j],"player"))) {
-							if (hasClass(childrens[j],"weapon")) {
-								for (k in weaponsArray) {
-									if (hasClass(childrens[j], weaponsArray[k].getName())) {
-										childrens[j].style.background = "url('img/" + weaponsArray[k].getRoadImage() + "')"  + " " +"no-repeat scroll 0 0 / 100% auto rgba(0, 0, 0, 0)";
-										childrens[j].className = childrens[j].className + " " + "road";
-									}
-								}
-							} else {
-								childrens[j].style.background = roadImage;
-								childrens[j].className = childrens[j].className + " " + "road";
-							}
-						} else {
-							return;
-						}
-					}
-				}
-			}
-			
+			perform2Loop(tabElmts.length);
 		} else if (tabElmts.length > 3 ) {
-			for (var i = 0; i < 3; i++) {
-				var childrens = tabElmts[i].children;
-				for (var j = 0; j < childrens.length; j++) {
-					//Vérication de l'index de l'élément actif par rapport aux différents éléments
-					if (j === activeElementIndex) {
-						if (!(hasClass(childrens[j],"wrongCell")) && !(hasClass(childrens[j],"player"))) {
-							if (hasClass(childrens[j],"weapon")) {
-								for (k in weaponsArray) {
-									if (hasClass(childrens[j], weaponsArray[k].getName())) {
-										childrens[j].style.background = "url('img/" + weaponsArray[k].getRoadImage() + "')"  + " " +"no-repeat scroll 0 0 / 100% auto rgba(0, 0, 0, 0)";
-										childrens[j].className = childrens[j].className + " " + "road";
-									}
-								}
-							} else {
-								childrens[j].style.background = roadImage;
-								childrens[j].className = childrens[j].className + " " + "road";
-							}
-						} else
-							return;
-					}
-				}
-			}
+			perform2Loop(3);
 		} else {
 			console.log("No road is possible to trace");
 		}
